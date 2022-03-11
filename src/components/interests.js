7837da427df90e61506c44b6d73426568be712a9
Jagroop <jagroopbagri5@gmail.com>
2022-03-10 17:29:01 -0800

    style updates

--- src/components/interests.js
+++ src/components/interests.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Interests() {
   return (
-    <div className="about">
+    <div className="interests">
       <h2>Interests</h2>
       <p>
         Interested in learning the latest technologies and frameworks to
